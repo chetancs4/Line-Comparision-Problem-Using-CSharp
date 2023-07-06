ee6e01ed0303c24cd046ac7fad1c9d2aaddd441f
Chetan <chetansadvelkar4@gmail.com>
2023-07-06 22:46:46 +0530

    Chetan: Calculate Length

---
+++ Length.cs
@@ -0,0 +1,27 @@
+﻿namespace LineComparisionProblems
+{
+    public class Length
+    {
+        public void LineLength()
+        {
+            Console.Write("Enter value of x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter value of y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter value of x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter value of y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            double lineLength = Math.Sqrt(dx * dx + dy * dy);
+
+            Console.WriteLine("Length is: " + lineLength);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LineComparisionProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------- Chetan ----------------");
+            Console.WriteLine("Welcome to Line Comparision Program!!");
+            Console.WriteLine("----------------------------------------");
+
+            Length length = new Length();
+            length.LineLength();
+        }
+    }
+}
